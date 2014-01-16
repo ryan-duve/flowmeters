e5d98c86e8e60c4fe4adc1fca889e9e8f1a5491a
root <uva-OptiPlex-GX620>
2014-01-15 22:17:02 -0500

    shows all four channels legend is in north west corner time is converted to EST from GMT through 18000 s fudge factor in both flow.php and index.html

--- flow.php
+++ flow.php
@@ -2,29 +2,53 @@
  $connect = mysql_connect("localhost","uva","uva1");
  mysql_select_db("slowcontrols",$connect);
  
- for($i=0;$i<4;$i++){
-	//http://stackoverflow.com/questions/4961524/mysql-query-latest-timestamp-unique-value-from-the-last-30-minutes
-	$flow_query[]="SELECT device AS label, created_at AS createdtime, measurement_reading AS measurement FROM usb1208ls WHERE device='d$i' AND created_at > (now() - interval 120 second)ORDER BY created_at DESC, id DESC";
+ //make one query for all data in last 2 minute
+ //http://stackoverflow.com/questions/4961524/mysql-query-latest-timestamp-unique-value-from-the-last-30-minutes
+ $flow_query="SELECT device AS d, created_at AS createdtime, measurement_reading AS measurement FROM usb1208ls WHERE created_at > (now() - interval 120 second)";
 
+ //run one query
+ $flow_result=mysql_query($flow_query) or die(mysql_error());
+
+ //go through results, stacking four data arrays
+
+ while($r=mysql_fetch_assoc($flow_result)){
+
+	//factor of 1K for javascript time, subtract 18K seconds for flot EST->GMT fudge factor
+ 	$ctime=(strtotime($r["createdtime"])-18000)*1000;
+
+	switch ($r["d"]){
+		case "d0": 
+			$data0[]="[\"".$ctime."\",\"".$r["measurement"]."\"]";
+			break;
+		case "d1": 
+			$data1[]="[\"".$ctime."\",\"".$r["measurement"]."\"]";
+			break;
+		case "d2": 
+			$data2[]="[\"".$ctime."\",\"".$r["measurement"]."\"]";
+			break;
+		case "d3": 
+			$data3[]="[\"".$ctime."\",\"".$r["measurement"]."\"]";
+			break;
+	}
  }
 
- for($i=0;$i<4;$i++){
-	$flow_result[]=mysql_query($flow_query[$i]) or die(mysql_error());
+ //echo "data0=".implode(',',$data0)."\n";
 
- }
+ //make output like:
+ //[	{ label:"Foo", data: [ [10, 1], [17, -14] },
+ // 	{ label:"Bar", data: [ [11, 13], [19, 11] }
+ //]
 
  $res="[";
- for($i=0;$i<2;$i++){
- 	$res.="{\"label\":\"d$i\",\"data\":[";
- 	while($r=mysql_fetch_assoc($flow_result[$i])){
- 	        $ctime=strtotime($r["createdtime"])*1000;
- 	        $res.="[$ctime,$r[measurement]],";
- 	}
- 	$res=substr($res,0,-1);
- 	$res.="]},";
- }
- $res=substr($res,0,-1);
- $res.="]";
+ $res.="{\"label\":\"separator\",\"data\":[";
+ $res.=implode(',',$data0);
+ $res.="]},{\"label\":\"screen\",\"data\":[";
+ $res.=implode(',',$data1);
+ $res.="]},{\"label\":\"he-3\",\"data\":[";
+ $res.=implode(',',$data2);
+ $res.="]},{\"label\":\"other\",\"data\":[";
+ $res.=implode(',',$data3);
+ $res.="]}]";
 
  echo $res;
 
