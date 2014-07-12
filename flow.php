df0e75714bf266b068d4dc9149d51560cb492b78
Ryan Duve <ryan.duve@gmail.com>
2014-07-12 10:56:46 -0400

    changed 120s to 1200s record request query

--- flow.php
+++ flow.php
@@ -4,7 +4,7 @@
  
  //make one query for all data in last 2 minute
  //http://stackoverflow.com/questions/4961524/mysql-query-latest-timestamp-unique-value-from-the-last-30-minutes
- $flow_query="SELECT device AS d, created_at AS createdtime, measurement_reading AS measurement FROM usb1208ls WHERE created_at > (now() - interval 120 second)";
+ $flow_query="SELECT device AS d, created_at AS createdtime, measurement_reading AS measurement FROM usb1208ls WHERE created_at > (now() - interval 1200 second)";
 
 
  //not getting anything back?  try running this
